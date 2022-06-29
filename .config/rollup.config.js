31b0ba20f44ffb30cc5eefa950c726664ae940bb
fish-ball <57082212@qq.com>
2022-06-29 20:49:33 +0800

    添加了 rollup.js 的打包脚本，支持通过 yarn build 实现打包 * 尚未解决 es5 输出的问题 * 尚未解决 babel decorator 语法支持的问题 * 未配置压缩

---
+++ .config/rollup.config.js
@@ -0,0 +1,10 @@
+export default [{
+  input: 'src/js/JsMind.js',
+  output: {
+    name: 'jsMind',
+    file: 'dist/jsmind.dist.js',
+    format: 'umd',
+    sourcemap: true,
+  }, plugins: [
+  ]
+}]
